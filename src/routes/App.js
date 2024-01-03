ce05695c697211e2ddaecb3a1345b1f51fafb334
“MonkeYor13” <monkeyor13@gmail.com>
2024-01-03 10:56:47 -0300

    starting class project

---
+++ src/routes/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+      />
+      <h1>Soy dev store</h1>
+    </div>
+  );
+}
+
+export default App;
